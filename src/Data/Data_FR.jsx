a052d76934e79a4804e7ff7c2cbcd8bc2897ca97
Mouad Rahmouni <mouadrah01@gmail.com>
2026-05-09 13:19:35 +0100

    correction avant l'hebergement

--- src/Data/Data_FR.jsx
+++ src/Data/Data_FR.jsx
@@ -18,7 +18,7 @@ import Hassan from "../Image/hassan.jpg";
 import AgnèsHumruzian from "../Image/Agnès-Humruzian.jpg";
 import fadhila from "../Image/fadhila.jpeg";
 // import françoise from "../Image/françoise.jpeg";
-import françoise from "../Image/françoise.jpeg";
+import françoise from "../Image/Françoise.jpeg";
 import fouad from "../Image/fouad.jpeg";
 import amal from "../Image/amal-ayouch.jpg";
 import lemjidi from "../Image/lemjidi-abdelkhalek.jpg";
