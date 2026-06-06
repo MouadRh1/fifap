5c0cd91d358af737207bc6956f10d833954006e3
Mouad Rahmouni <mouadrah01@gmail.com>
2026-06-06 20:35:26 +0100

    suppression d'activite

--- src/component/Pages/navBar.jsx
+++ src/component/Pages/navBar.jsx
@@ -266,7 +266,7 @@ export default function Navbar() {
                 className="flex items-center gap-2 px-4 py-2 bg-gradient-to-r from-[#ac5f2d] to-[#e67e22] hover:shadow-lg text-white rounded-lg transition-all duration-300"
               >
                 <LogIn size={18} />
-                <span className="text-sm font-medium">Admin</span>
+                <span className="text-sm font-medium">Login</span>
               </Link>
             )}
           </div>
